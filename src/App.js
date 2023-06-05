ea2190bfb49111ec76582fd5b8b3eff3f9f7b314
karthiktr27597 <karthiktr27597@gmail.com>
2023-06-05 12:52:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import SideBar from './Components/SideBar';
+import HeaderBar from './Components/HeaderBar';
+import Footer from './Components/Footer';
+import Mainbar from './Components/Mainbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mainbar />
     </div>
   );
 }
